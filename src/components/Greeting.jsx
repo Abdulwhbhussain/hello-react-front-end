c39d1f884a51c25a82f7121207d7a382405e70ba
WHB901 <abdulwhbhussain@gmail.com>
2023-11-29 00:29:54 +0500

    complete frontend

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,6 +1,55 @@
 // Greeting.jsx
-import React from 'react';
 
-const Greeting = () => <div>Hello, welcome to my app!</div>;
+// import React from 'react';
+
+// const Greeting = () => <div>Hello, welcome to my app!</div>;
+
+// export default Greeting;
+
+// import React, { useEffect } from 'react';
+// import { Routes, Route} from 'react-router-dom';
+// import Greeting from './Greeting';
+// import { useDispatch } from 'react-redux';
+// import { getGreeting } from '../greeting/greetingSlice';
+
+// const App = () => {
+//   const dispatch = useDispatch()
+//   useEffect(() => {
+//     dispatch(getGreeting())
+//   }, [])
+//   return (
+//     <main>
+//       <Routes>
+//         <Route path="/" element={<Greeting />} />
+//       </Routes>
+//     </main>
+//   )
+// }
+// export default App
+
+// Greeting.jsx
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getGreeting } from '../store/greeting/greetingSlice';
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const greetingMessage = useSelector((state) => state.greeting);
+
+  useEffect(() => {
+    dispatch(getGreeting());
+  }, [dispatch]);
+
+  const handleButtonClick = () => {
+    dispatch(getGreeting());
+  };
+
+  return (
+    <div>
+      <p>{greetingMessage}</p>
+      <button type="button" onClick={handleButtonClick}>Change Message</button>
+    </div>
+  );
+};
 
 export default Greeting;
---
+++ src/store/greeting/greetingSlice.js
@@ -0,0 +1,44 @@
+// slices/greetingSlice.js
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  greeting: '',
+  isLoading: true,
+  isError: false,
+  errorMsg: '',
+};
+
+const url = 'http://localhost:3000/api/v1/greetings/random_greeting';
+
+export const getGreeting = createAsyncThunk('greeting/fetchGreeting', async (_, thunkAPI) => {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    const errorMsg = `${error.code}: ${error.message}`;
+    return thunkAPI.rejectWithValue(errorMsg);
+  }
+});
+
+const greetingSlice = createSlice({
+  name: 'greeting',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(getGreeting.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getGreeting.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.greeting = action.payload;
+      })
+      .addCase(getGreeting.rejected, (state, action) => {
+        state.isError = true;
+        state.errorMsg = action.payload;
+        state.isLoading = false;
+      });
+  },
+});
+
+export default greetingSlice.reducer;
